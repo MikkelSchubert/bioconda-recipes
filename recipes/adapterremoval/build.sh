e139385439c6cf2de2af13956c8a3eef3d2c2753
Mikkel Schubert <2411627+MikkelSchubert@users.noreply.github.com>
2026-07-09 14:39:44 +0200

    attempt to fix sphinx failure

--- recipes/adapterremoval/build.sh
+++ recipes/adapterremoval/build.sh
@@ -6,6 +6,9 @@ set -euo pipefail
 # build, silences warnings, and preserves the higher hardening level
 export CPPFLAGS="-U_FORTIFY_SOURCE ${CPPFLAGS} -U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=3"
 
+# Required to build documentation with sphinx
+export LC_ALL="en_US.UTF-8"
+
 meson setup "build" --prefix="${PREFIX}" 
 meson compile -C "build" adapterremoval3
 meson install -C "build"
